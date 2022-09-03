d0e0376c5f8d0d7ed70271320dcb91df673f1b92
edmilsonhora <38793820+edmilsonhora@users.noreply.github.com>
2022-09-03 10:24:43 -0300

    Implementações

---
+++ src/ESH-Modelo.ApplicationService/Views/ViewBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ESH_Modelo.ApplicationService.Views
+{
+   public abstract class ViewBase
+    {
+        public int Id { get; set; }
+    }
+}
