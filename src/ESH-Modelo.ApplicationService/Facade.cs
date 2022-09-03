426e6681d0c675db483db9fcf40f6737aa4db11c
edmilsonhora <38793820+edmilsonhora@users.noreply.github.com>
2022-09-03 10:24:43 -0300

    Implementações

---
+++ src/ESH-Modelo.ApplicationService/Facade.cs
@@ -0,0 +1,13 @@
+﻿using ESH_Modelo.ApplicationService.Facades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ESH_Modelo.ApplicationService
+{
+    public class Facade : IFacade
+    {
+    }
+}
